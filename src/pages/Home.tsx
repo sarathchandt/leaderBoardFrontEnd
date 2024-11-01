8626801f6996d81c7e2b9584b98fd7915133789e
sarathchandt <sarathchand770@gmail.com>
2024-11-01 15:43:36 +0530

    some changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -62,7 +62,7 @@ const Home = () => {
 
   return (
     <OnBoardingTemplate
-      mainTitle={rank  + " "+leaderBoard[currentPosition]?.name}
+      mainTitle={leaderBoard[currentPosition]?.name  ?(rank  + " "+leaderBoard[currentPosition]?.name):""}
       firstRoundGradientStyle="absolute md:top-[10%] top-[15%]   left-[10%] md:left-0  rotate-90  bg-gradient-to-r from-indigo-500 to-purple-990 "
       secondRoundGradientStyle="absolute bottom-[10%]  right-[10%]  rotate-[40deg]  bg-gradient-to-r from-indigo-900 to-purple-990 "
     >
